3072f2fe7e8f5c33e5e535fd293b0329197b87c3
Sleaker <sleaker@gmail.com>
2011-09-26 09:29:42 -0700

    simple SuperPerms fix.

--- src/lishid/orebfuscator/utils/PermissionRelay.java
+++ src/lishid/orebfuscator/utils/PermissionRelay.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package lishid.orebfuscator.utils;
 
 import org.bukkit.entity.Player;
@@ -11,8 +10,7 @@ import com.nijikokun.bukkit.Permissions.Permissions;
 public class PermissionRelay {
     public static PermissionHandler handler;
     
-    public static void Setup(PluginManager pm)
-    {
+    public static void Setup(PluginManager pm) {
         if (handler == null) {
             if (pm.getPlugin("Permissions") != null) {
             	handler = ((Permissions) pm.getPlugin("Permissions")).getHandler();
@@ -22,48 +20,11 @@ public class PermissionRelay {
         }
     }
     
-	public static boolean hasPermission(Player player, String permission)
-	{
+	public static boolean hasPermission(Player player, String permission) {
 		if (handler == null) {
-			return player.isOp();
-        }else{
+			return player.isOp() ? true : player.hasPermission(permission);
+        } else {
         	return handler.has(player, permission);
         }
 	}
-
 }
-=======
-package lishid.orebfuscator.utils;
-
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginManager;
-
-import com.nijiko.permissions.PermissionHandler;
-import com.nijikokun.bukkit.Permissions.Permissions;
-
-
-public class PermissionRelay {
-    public static PermissionHandler handler;
-    
-    public static void Setup(PluginManager pm)
-    {
-        if (handler == null) {
-            if (pm.getPlugin("Permissions") != null) {
-            	handler = ((Permissions) pm.getPlugin("Permissions")).getHandler();
-            } else {
-                //log.info("Permission system not detected, defaulting to OP");
-            }
-        }
-    }
-    
-	public static boolean hasPermission(Player player, String permission)
-	{
-		if (handler == null) {
-			return player.isOp();
-        }else{
-        	return handler.has(player, permission);
-        }
-	}
-
-}
->>>>>>> f2a74dba45cffa3cfa52db44f62e029351ddccac
