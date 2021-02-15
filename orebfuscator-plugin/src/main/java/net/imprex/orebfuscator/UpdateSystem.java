f765fa68016acbd6fdc9ef99f0cba03d44874f9a
Maurice Eisenblätter <Ingrim4@users.noreply.github.com>
2021-02-15 18:59:01 +0100

    [#97] Async UpdateSystem (#98)
    * feat: async UpdateSystem
    
    * feat: UpdateSystem failed attempts counter

--- orebfuscator-plugin/src/main/java/net/imprex/orebfuscator/UpdateSystem.java
+++ orebfuscator-plugin/src/main/java/net/imprex/orebfuscator/UpdateSystem.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import com.google.gson.JsonObject;
@@ -13,6 +16,7 @@ import com.google.gson.JsonParser;
 
 import net.imprex.orebfuscator.config.GeneralConfig;
 import net.imprex.orebfuscator.util.OFCLogger;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
@@ -20,12 +24,16 @@ import net.md_5.bungee.api.chat.HoverEvent;
 public class UpdateSystem {
 
 	private static final String API_LATEST = "https://api.github.com/repos/Imprex-Development/Orebfuscator/releases/latest";
+	private static final long UPDATE_COOLDOWN = 1_800_000L; // 30min
+
+	private final Lock lock = new ReentrantLock();
 
 	private final Orebfuscator orebfuscator;
 	private final GeneralConfig generalConfig;
 
 	private JsonObject releaseData;
-	private long updateTimestamp = -1;
+	private long updateCooldown = -1;
+	private int failedAttempts = 0;
 
 	public UpdateSystem(Orebfuscator orebfuscator) {
 		this.orebfuscator = orebfuscator;
@@ -35,21 +43,40 @@ public class UpdateSystem {
 	}
 
 	private JsonObject getReleaseData() {
-		long systemTime = System.currentTimeMillis();
-		if (this.releaseData != null || systemTime - this.updateTimestamp > 1_800_000) {
-			try {
-				URL url = new URL(API_LATEST);
-				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-				try (InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream())) {
-					this.releaseData = new JsonParser().parse(inputStreamReader).getAsJsonObject();
-					this.updateTimestamp = systemTime;
+		this.lock.lock();
+		try {
+			long systemTime = System.currentTimeMillis();
+
+			if (this.failedAttempts < 5) {
+
+				if (this.releaseData != null || systemTime - this.updateCooldown > UPDATE_COOLDOWN) {
+					try {
+						URL url = new URL(API_LATEST);
+						HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+						try (InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream())) {
+							this.releaseData = new JsonParser().parse(inputStreamReader).getAsJsonObject();
+							this.updateCooldown = systemTime;
+						}
+					} catch (IOException e) {
+						OFCLogger.warn("Unable to fetch latest update from: " + API_LATEST);
+						OFCLogger.warn(e.toString());
+
+						if (++this.failedAttempts == 5) {
+							this.updateCooldown = systemTime;
+						}
+					}
 				}
-			} catch (IOException e) {
-				OFCLogger.warn("Unable to fetch latest update from: " + API_LATEST);
-				OFCLogger.warn(e.toString());
+
+			} else if (systemTime - this.updateCooldown > UPDATE_COOLDOWN) {
+				this.failedAttempts = 0;
+				this.updateCooldown = -1;
+				return this.getReleaseData();
 			}
+
+			return this.releaseData;
+		} finally {
+			this.lock.unlock();
 		}
-		return this.releaseData;
 	}
 
 	private String getTagName() {
@@ -69,29 +96,39 @@ public class UpdateSystem {
 	}
 
 	private boolean isUpdateAvailable() {
-		String tagName = this.getTagName();
-		String version = this.orebfuscator.getDescription().getVersion();
-		return this.generalConfig.checkForUpdates() && tagName != null && !version.equals(tagName);
+		if (this.generalConfig.checkForUpdates()) {
+			String tagName = this.getTagName();
+			String version = this.orebfuscator.getDescription().getVersion();
+			return tagName != null && !version.equals(tagName);
+		}
+		return false;
 	}
 
 	private void checkForUpdates() {
-		if (this.isUpdateAvailable()) {
-			String url = " " + this.getHtmlUrl() + " ";
-			int lineLength = (int) Math.ceil((url.length() - 18) / 2d);
-			String line = StringUtils.repeat("=", lineLength);
+		Bukkit.getScheduler().runTaskAsynchronously(this.orebfuscator, () -> {
+			if (this.isUpdateAvailable()) {
+				String url = " " + this.getHtmlUrl() + " ";
+				int lineLength = (int) Math.ceil((url.length() - 18) / 2d);
+				String line = StringUtils.repeat("=", lineLength);
 
-			OFCLogger.warn(line + " Update available " + line);
-			OFCLogger.warn(url);
-			OFCLogger.warn(StringUtils.repeat("=", lineLength * 2 + 18));
-		}
+				OFCLogger.warn(line + " Update available " + line);
+				OFCLogger.warn(url);
+				OFCLogger.warn(StringUtils.repeat("=", lineLength * 2 + 18));
+			}
+		});
 	}
 
 	public void checkForUpdates(Player player) {
-		if (this.isUpdateAvailable()) {
-			player.spigot().sendMessage(new ComponentBuilder("[§bOrebfuscator§f]§7 A new release is available ")
-					.append("§f§l[CLICK HERE]")
-					.event(new ClickEvent(ClickEvent.Action.OPEN_URL, this.getHtmlUrl()))
-					.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("§7Click here to see the latest release").create())).create());
-		}
+		Bukkit.getScheduler().runTaskAsynchronously(this.orebfuscator, () -> {
+			if (this.isUpdateAvailable()) {
+				BaseComponent[] components = new ComponentBuilder("[§bOrebfuscator§f]§7 A new release is available ")
+						.append("§f§l[CLICK HERE]")
+						.event(new ClickEvent(ClickEvent.Action.OPEN_URL, this.getHtmlUrl()))
+						.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("§7Click here to see the latest release").create())).create();
+				Bukkit.getScheduler().runTask(this.orebfuscator, () -> {
+					player.spigot().sendMessage(components);
+				});
+			}
+		});
 	}
 }
